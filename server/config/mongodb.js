4ae340049668bc6fabe354ab7872fd19c207232d
Rasmy-r7 <rasmy5741@gmail.com>
2025-01-12 14:42:53 +0530

    connected change

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
 
 
-    mongoose.connection.on('conected', ()=>console.log("Database Connected"))
+    mongoose.connection.on('connected', ()=>console.log("Database Connected"))
 
 
     await mongoose.connect(`${process.env.MONGODB_URI}/Authentication-system`)
