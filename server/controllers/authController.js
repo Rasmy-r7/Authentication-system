8bdaea9b298fdf28908e8f211ee1942c3f1b184c
Rasmy-r7 <rasmy5741@gmail.com>
2025-01-24 21:03:34 +0530

    login in backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,6 +29,7 @@ export const register = async (req, res) => {
             maxAge: 7 * 24 * 60 * 1000
 
         })
+        return res.json({success: true});
 
     } catch (error) {
         res.json({ success: false, message: error.message })
@@ -36,3 +37,34 @@ export const register = async (req, res) => {
 }
 
 
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({ success: false, message: 'Email and password are required' })
+    }
+    try {
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, message: 'Invalid email' })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.json({ success: false, message: 'Invalid password' })
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expireIn: '7d' })
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ?
+                'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 1000
+
+        })
+        return res.json({success: true});
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
+    }
+}
