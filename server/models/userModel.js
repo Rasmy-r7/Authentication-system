a62e7a10d99c7bea183d9b20c20ea7f54ed7c351
Rasmy-r7 <rasmy5741@gmail.com>
2025-01-24 17:29:17 +0530

    userModel

---
+++ server/models/userModel.js
@@ -0,0 +1,19 @@
+import { Password } from "@mui/icons-material";
+import { verify } from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {type:String, required: true},
+    email: {type:String, required: true, unique: true},
+    Password: {type:String, required: true},
+    verifyOtp: {type:String, default: ''},
+    verifyOtpExpireAt: {type:Number, default: 0},
+    isAccountVerified: {type: Boolean, default: false},
+    resetOtp: {type: String, default: ''},
+    resetOtpExpireAt: {type: Number, default: 0},
+})
+
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+
+export default userModel;
